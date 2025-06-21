12451b7b17b66e7cbec259ed32deb06c95d734a0
pazliddinovulugbek <pazliddinovulugbek07@gmail.com>
2025-06-21 11:06:02 +0500

    menu btn full

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Style/Navbar.css'
 function Navbar() {
+  const [show, setShow] = useState(false)
+
+  const handShow = () => {
+    setShow(prev => !prev)
+  }
   return (
     <>
       <nav>
         <h1>UP<span>code</span></h1>
-        <div className="center-nav">
+        <div className={`center-nav ${show ? 'show-mobile' : ''}`}>
           <a href="">Home</a>
           <a href="">Skills</a>
           <a href="">About Us</a>
@@ -15,10 +20,10 @@ function Navbar() {
         <div className="end-nav-btn">
           <button>Let's Talk</button>
         </div>
-        <div className="menu">
+        <div className="menu" onClick={handShow}>
           <button>Menu</button>
         </div>
-      </nav>
+      </nav >
 
     </>
   )
